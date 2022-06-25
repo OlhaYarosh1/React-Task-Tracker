1ecadbf23f6b5b1905779296f1e79fa78f75d99d
Olha Yarosh <olha.yarosh1@gmail.com>
2022-06-25 03:38:48 +0300

    Made the button open and close the Form.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState(
     [
         {
@@ -42,10 +43,19 @@ const toggleReminder = (id) => {
   )
 }
 
+// Add Task
+const addTask = (task) => {
+  const id = Math.floor(Math.random()*10000) + 1
+  const newTask ={ id, ...task }
+  setTasks([...tasks, newTask])
+}
+
   return (
     <div className='container'>
-      <Header />
-      <AddTask />
+      <Header onAdd={() => 
+        setShowAddTask(!showAddTask)} 
+        showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length >0 ? ( 
       <Tasks tasks={tasks} 
       onDelete={deleteTask} onToggle={toggleReminder} />
